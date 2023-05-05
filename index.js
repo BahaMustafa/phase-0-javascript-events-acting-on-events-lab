33589851a929f723d0a2ba6683d9bda94debe491
= <=>
2023-05-04 21:13:34 -0700

    acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+// index.js
+const DODGER = document.getElementById('dodger'); 
+
+function moveDodgerLeft() {
+  const currentPosition = parseInt(DODGER.style.left, 10);
+
+  if (currentPosition > 0) {
+    DODGER.style.left = `${currentPosition - 1}px`;
+  }
+}
+
+// Usage of the function
+moveDodgerLeft();
+
+// index.js
+const DODGER1 = document.getElementById('dodger'); 
+const GAME_WIDTH = 400; 
+
+
+function moveDodgerRight() {
+  const currentPosition = parseInt(DODGER1.style.left, 10);
+  const dodgerWidth = parseInt(DODGER1.offsetWidth, 10);
+  const rightEdge = GAME_WIDTH - dodgerWidth;
+
+  if (currentPosition < rightEdge) {
+    DODGER1.style.left = `${currentPosition + 1}px`;
+  }
+}
+
+// Usage of the function
+moveDodgerRight();
